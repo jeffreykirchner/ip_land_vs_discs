2477784d3a0b1c8cdcc2f2f1a0f71f68a727a94c
Jeffrey Kirchner <jkirchner@gmail.com>
2024-01-30 16:08:41 -0800

    update test mode.

--- main/templates/subject/subject_home/subject_home.js
+++ main/templates/subject/subject_home/subject_home.js
@@ -475,7 +475,7 @@ var app = Vue.createApp({
             if(message_data.period_is_over)
             {
                 app.setup_disc_inventory();     
-                if(app.session.parameter_set.test_mode) app.test_mode_reset_info();    
+                if(app.session.parameter_set.test_mode=="True") app.test_mode_reset_info();    
             }
 
             app.setup_seed_inventory();
