3fe0e22377f1fabb5bfd25661c07bc928519db64
Jeffrey Kirchner <jkirchner@gmail.com>
2024-02-05 13:31:53 -0800

    can't move on top of other players. fixes #93.

--- main/templates/subject/subject_home/the_stage/subject.js
+++ main/templates/subject/subject_home/the_stage/subject.js
@@ -45,6 +45,20 @@ get_offset:function get_offset()
                         null);
         return;
     }
+
+    //can't move ontop of other players
+    for(i in app.session.world_state.session_players)
+    {
+        let obj = app.session.world_state.session_players[i];
+    
+        if(obj.id == app.session_player.id) continue;
+
+        if(app.get_distance(obj.current_location, local_pos) < 100 &&
+        app.get_distance(obj.current_location, local_player.current_location) <= app.session.parameter_set.interaction_range+125)
+        {            
+            return;
+        }
+    }
     
     local_player.target_location.x = local_pos.x;
     local_player.target_location.y = local_pos.y;
