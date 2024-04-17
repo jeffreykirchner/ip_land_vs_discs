149ff98b47647f42887bbe3c2e60e43d888698a1
Jeffrey Kirchner <jkirchner@gmail.com>
2024-04-17 16:29:23 -0700

    do not preselect any discs to send. fixes #131.

--- main/templates/subject/subject_home/the_stage/avatar.js
+++ main/templates/subject/subject_home/the_stage/avatar.js
@@ -386,14 +386,14 @@ start_send_disc: function start_send_disc()
     {
         if(session_player.disc_inventory[i])
         {
-            if(i == app.session_player.id)
-            {
-                app.selected_player.interaction_discs[i] = true;
-            }
-            else
-            {
+            // if(i == app.session_player.id)
+            // {
+            //     app.selected_player.interaction_discs[i] = true;
+            // }
+            // else
+            // {
                 app.selected_player.interaction_discs[i] = false;
-            }
+            //}
         }
     }
 
